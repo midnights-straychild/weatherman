823bbe0ccc5c11fd5e33bc487f1993b0c6bdbc1e
Ralf Lauseker <rlauseker@loyaltypartner.com>
2017-11-07 16:56:58 +0100

    Init

---
+++ db.py
@@ -0,0 +1,17 @@
+""" Postgres """
+import psycopg2
+
+def connect():
+    """ Connect to DB """
+    print "connect"
+    connection = psycopg2.connect("dbname=weatherman user=postgres password=postgres")
+
+    return connection
+
+def version(connection):
+    """ Returns Version """
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT version()')
+
+    return cursor.fetchone()
---
+++ test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+""" DB Module """
+import db
+from flask import Flask
+
+def init():
+    """ Bootstrapping """
+    print "init"
+    initFlask()
+
+def initFlask():
+    """ Init Web Layer """
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return db.version(db.connect())
+
+    @app.route('/cakes')
+    def cakes():
+        return 'Yummy cakes!'
+
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0')
+
+init()
