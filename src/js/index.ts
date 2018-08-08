b1aa435b61b9217b2b0951f736031cb58b06839f
Ralf Lauseker <ralf.lauseker@gmail.com>
2018-08-08 08:59:55 +0200

    added typescript and webpack

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,25 +1,14 @@
 /// <reference path="../../node_modules/@types/jquery/index.d.ts" />
 
-import '../../node_modules/jquery/dist/jquery.min.js';
-import * as $ from 'jquery';
-import '../../node_modules/jquery/dist/jquery.min.map';
-import '../node_modules/popper.js/dist/popper.min.js';
-import '../node_modules/popper.js/dist/popper.min.js.map';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import '../node_modules/flot/jquery.flot.js';
-import '../node_modules/flot/jquery.flot.time.js';
+import 'flot';
+import '../../node_modules/flot/jquery.flot.time';
+
+import '../sass/main.sass';
 
 
 $(document).ready(function () {
     let oneWeekAgo = new Date();
     oneWeekAgo.setDate(oneWeekAgo.getDate() - 3);
-
-//        let sensors = {{ get_sensors()|safe }};
-    let data1: any[] = [];
-    let data2: any[] = [];
-//        let data1 = {{ get_sensordata(1)|safe }};
-
-//        let data2 = {{ get_sensordata(2)|safe }};
 
     let options = {
         series: {
@@ -35,12 +24,12 @@ $(document).ready(function () {
         },
         yaxis: {
             minTickSize: [10],
-            tickFormatter: function (number, object) {
+            tickFormatter: (number: number, object: any) => {
                 return number + '°C';
             }
         }
     };
 
-    $.plot($("#chart"), [data1], options);
-    $.plot($("#chart2"), [data2], options);
+    $.plot($("#chart"), [$("#chart").data('points')], options);
+    $.plot($("#chart2"), [$("#chart2").data('points')], options);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,42 @@
+/* jshint node: true, esversion: 6 */
 const path = require('path');
+const webpack = require('webpack');
+// `CheckerPlugin` is optional. Use it if you want async error reporting.
+// We need this plugin to detect a `--watch` mode. It may be removed later
+// after https://github.com/webpack/webpack/issues/3460 will be resolved.
+const {CheckerPlugin} = require('awesome-typescript-loader');
 
 module.exports = {
-    entry: './src/js/index.ts',
+    //context: __dirname + '/src',
+    entry: __dirname + '/src/js/index.ts',
     module: {
         rules: [
             {
+                test: /\.sass$/,
+                use: 'fast-sass-loader'
+            },
+            {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
+                use: 'awesome-typescript-loader'
             }
         ]
     },
+    plugins: [
+        new CheckerPlugin(),
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ],
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        descriptionFiles: ['package.json'],
+        modules: ['node_modules'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.css']
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'static')
-    }
+    },
+    devtool: 'source-map'
 };
